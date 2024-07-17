245acd7a9c1388e32c63aae6d0b62b20de0abd0f
Matthew Kaye <mattkaye79@gmail.com>
2024-07-16 23:25:48 -0400

    Adding start battle button.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import Grid from "./components/Grid";
 function App() {
   return (
     <main className="my-12 container mx-auto">
-      <div className="flex w-full flex-col items-center">
+      <div className="flex w-full flex-col items-center gap-6">
         <img src={GOTLogo} alt="Game of Thrones Logo" />
-        <h1 className="text-6xl text-orange-500 mt-6">Grid Survival Game!</h1>
+        <h1 className="text-6xl text-orange-500 fun-font">
+          Grid Survival Game!
+        </h1>
+        <p className="max-w-xl text-white text-center text-xl">
+          How long can you survive the Lannister's and their allies? Play as Jon
+          Snow and use your wits to out maneuver your opponents for as long as
+          possible using your arrow keys. (&uarr; &darr; &larr; &rarr;)
+        </p>
+        <button className="start-battle">⚔️ Ready For Battle?</button>
       </div>
       <section>
         <Grid />
